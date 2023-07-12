fb272e1848c207ba289f1ba255709384511623e4
Eunesh <87225307+Eunesh@users.noreply.github.com>
2023-07-12 23:28:41 +0545

    Initialize  MediaStreaming API

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,28 +1,46 @@
-async function getPermission() {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    console.log("getUserMedaia Supported");
+// Setting varibale
+const record = document.getElementById("record_btn"); // getting record_btn as id
+const stop = document.getElementById("stop_btn");
 
-    const Stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
+stop.style.display = "none"; // initially stop button should not be displayed, We should show stop button only when recording started
 
-    const mediaRecorder = new MediaRecorder(Stream); // MediaStreaming Recordng API new Object
-    return mediaRecorder;
-  } else {
-    console.log("getUserMedia not supported on your browser!");
+// function for getting userMedia
+async function getMedia(constraints) {
+  try {
+    let stream = await navigator.mediaDevices.getUserMedia(constraints);
+    return stream;
+  } catch (err) {
+    console.log(err);
   }
 }
 
-function Record() {
-  getPermission().then((result) => {
-    //   result.start();
-    console.log(result);
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  console.log("getUserMedaia Supported");
+  let constraints = { audio: true };
+  let audio_chunks = []; // initializing  audio_chunks as emmpty array
+
+  getMedia(constraints).then((stream) => {
+    onSuccess(stream);
   });
 
-  //   const elem = document.getElementById("Title");
-  //   let TextColor = elem.style.color;
-}
+  function onSuccess(Stream) {
+    const mediaRecorder = new MediaRecorder(Stream); // MediaStreaming Recordng API new Object
 
-function Stop() {
-  console.log("Stop THe record");
+    record.onclick = function () {
+      record.style.display = "none";
+      stop.style.display = "inline";
+
+      mediaRecorder.start();
+      console.log(mediaRecorder);
+    };
+
+    stop.onclick = function () {
+      record.style.display = "inline";
+      stop.style.display = "none";
+      mediaRecorder.stop();
+      console.log(mediaRecorder);
+    };
+  }
+} else {
+  console.log("getUserMedia not supported on your browser!");
 }
