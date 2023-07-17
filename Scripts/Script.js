1ccbce4c3f4fe7ee40407ed58409579961291de5
Eunesh <87225307+Eunesh@users.noreply.github.com>
2023-07-17 23:33:54 +0545

    Almost Done

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,6 +1,8 @@
 // Setting varibale
 const record = document.getElementById("record_btn"); // getting record_btn as id
 const stop = document.getElementById("stop_btn");
+const card = document.querySelector(".listining_card");
+const countup = document.getElementById("countup");
 
 stop.style.display = "none"; // initially stop button should not be displayed, We should show stop button only when recording started
 
@@ -27,44 +29,53 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // Main Function
   function onSuccess(Stream) {
     const mediaRecorder = new MediaRecorder(Stream); // MediaStreaming Recordng API new Object
+    var seconds = 0;
+
+    // For Timer
+    function setTime() {
+      ++seconds;
+      let hour = Math.floor(seconds / 3600);
+
+      let minute = Math.floor((seconds - hour * 3600) / 60);
+
+      updSecond = seconds - (hour * 3600 + minute * 60);
+
+      countup.innerHTML = hour + ":" + minute + ":" + updSecond;
+    }
 
     // After clicking record button
     record.onclick = function () {
       record.style.display = "none";
       stop.style.display = "inline";
-
+      card.style.display = "flex";
       mediaRecorder.start();
-      console.log(mediaRecorder);
+      setInterval(setTime, 1000);
     };
 
     // After clicking stop button
     stop.onclick = function () {
       record.style.display = "inline";
       stop.style.display = "none";
+      card.style.display = "none";
       mediaRecorder.stop();
-      console.log(mediaRecorder);
+      seconds = 0;
     };
 
     mediaRecorder.onstop = function (e) {
       const audio = document.createElement("audio");
-      // const p = document.createElement("p");
-      // p.innerHTML = "This is a paragraph.";
-
       audio.setAttribute("controls", "");
-
       audio.controls = true;
       const blob = new Blob(audio_chunks, { type: "audio/ogg; codecs=opus" });
       console.log(blob);
-      // audio_chunks = [];
+      audio_chunks = [];
       const audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
-      document.getElementById("main_section").appendChild(audio);
+      document.getElementById("section").appendChild(audio);
     };
 
-    // if data is available in our Media Recorder then in here we push to audio_chunks array data is on BLOB data types
+    // if data is available in our Media Recorder then in here we push to audio_chunks(array).
     mediaRecorder.ondataavailable = function (e) {
       audio_chunks.push(e.data);
-      console.log(audio_chunks);
     };
   }
 } else {
